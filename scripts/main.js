dc7ab187024dd52a99387588a9d9c34ec745c623
williamweldon <32897997+williamweldon@users.noreply.github.com>
2017-11-07 11:17:34 +1000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Arsenal is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
